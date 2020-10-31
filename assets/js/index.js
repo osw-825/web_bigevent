3891e8575e36a34611b1cb033033b7d842347aab
hl <18974241028@163.com>
2020-10-31 15:00:23 +0800

    完成了user功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,32 +14,31 @@ $(function() {
 
     })
 
-    function getUserInfo() {
-        $.ajax({
-            method: 'GET',
-            url: '/my/userinfo',
-            success: function(res) {
-                if (res.status !== 0) {
-                    return layer.msg("获取用户失败")
-                }
-                renderAvatar(res.data)
-            }
-        })
-    }
-
-    //用户头像
-    function renderAvatar(user) {
-        var name = user.username || user.nickname;
-        $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
-        if (user.user_pic !== null) {
-            $('.layui-nav-img').attr('src', user.user_pic).show();
-            $('.text-avatar').hide();
-        } else {
-            $('.layui-nav-img').hide();
-            var first = name[0].toUpperCase();
-            $('.text-avatar').html(first).show()
-        }
-    }
-
-
 })
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg("获取用户失败")
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+//用户头像
+function renderAvatar(user) {
+    var name = user.username || user.nickname;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show()
+    }
+}
